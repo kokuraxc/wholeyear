0496e3243727e377a784eddfd1a59a0d3a2bd0c1
Guo Zhengjin <guo.zhengjin@gmail.com>
2019-08-09 17:35:25 +0800

    clear default ui. show grid view.

--- lib/main.dart
+++ lib/main.dart
@@ -71,40 +71,30 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 10,
+          childAspectRatio: 1.0,
+          padding: const EdgeInsets.all(4.0),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          children: <String>[
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+            'https://www.google.com.sg/logos/doodles/2019/singapore-national-day-2019-5096755438813184-law.gif',
+          ].map((String url) {
+            return new GridTile(
+                child: new Image.network(url, fit: BoxFit.cover));
+          }).toList(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
